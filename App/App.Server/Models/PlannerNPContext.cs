0ae213d5115ab9c6e89c7cf3cad3a3b245534ea3
Marie Korandová <mariekorandova@gmail.com>
2024-06-14 23:35:23 +0200

    Created project outline

---
+++ App/App.Server/Models/PlannerNPContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.Server.Models
+{
+    public class PlannerNPContext : DbContext
+    {
+        public DbSet<Ranger> Rangers { get; set; }
+
+        public PlannerNPContext(DbContextOptions options) : base(options) {}
+    }
+}
---
+++ App/App.Server/Program.cs
@@ -0,0 +1,50 @@
+
+using Microsoft.EntityFrameworkCore;
+using App.Server.Models;
+
+namespace App.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<PlannerNPContext>(options =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("StudentsConnection");
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
